dbc8f941ea61c43df6fd291c6c88120a1cb3b854
sajad_bana <sajadbanazadeh@gmail.com>
2024-04-22 16:44:33 +0330

    add library ast for read dict from env file

--- run_modbus.py
+++ run_modbus.py
@@ -9,4 +9,18 @@ import pandas as pd
 from datetime import datetime
 from multiprocessing import Pool
 from dotenv import load_dotenv
+import ast
+# === load data ============================================
+load_dotenv()
+dict_ip_address_str = os.getenv("dict_ip_address")
+dict_usual_parameters_str = os.getenv("dict_usual_parameters")
 
+dict_ip_address = ast.literal_eval(dict_ip_address_str)
+dict_usual_parameters = ast.literal_eval(dict_usual_parameters_str)
+
+# print("*"*50)
+# print(dict_ip_address)
+# print("*"*50)
+# print(dict_usual_parameters)
+
+# === 
